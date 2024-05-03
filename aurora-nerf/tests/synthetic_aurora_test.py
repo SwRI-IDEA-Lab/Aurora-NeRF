2c3d67924980d298cf1988f612e1c4971e29c91b
Andrés Muñoz-Jaramillo <andres.munoz@swri.org>
2024-05-02 18:49:22 -0600

    Initialize synthetic aurora tests

---
+++ aurora-nerf/tests/synthetic_aurora_test.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+import numpy as np
+
+
+class SyntheticAuroraTest(unittest.TestCase):
+
+    def setUp(self):
+
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
